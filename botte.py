cc8d81e33af6b4aa0844cae79ed1dab678a9e990
Galyfray <galyfray@gmail.com>
2019-06-20 22:48:44 +0200

    la cmd tierSet écris correctement reste la rétros activiter des tier

--- botte.py
+++ botte.py
@@ -3,27 +3,55 @@ from discord.ext import tasks, commands
 from discord.ext.commands import Bot
 import asyncio
 import os
-
+import json
 bot=commands.Bot(command_prefix="bot!")
+
+#Définition des fonctions:
+
+def I_conf(guildName: str):
+    if not(os.path.isdir("./" + guildName)):
+        os.mkdir("./" + guildName)
+    if not(os.path.exists("./" + guildName + "/server.json")):
+        with open("./" + guildName+ "/server.json","w") as f:
+            f.write("{ }")
+            f.close()
+     
+
 
 @bot.command()
 async def tierSet(ctx,tier):
     C=[]
+    try:
+        int(tier)
+    except:
+        await ctx.send("usage de la commande: bot!tierSet (int)tier @role @role  ... ")
+        return
+    
     if len(ctx.message.role_mentions)==0:
         await ctx.send("usage de la commande: bot!tierSet (int)tier @role @role  ... \nle rôle doit pouvoir être mentioner")
-    elif type(tier) is int:
+    else:
         for r in ctx.message.role_mentions:
             C.append(r.mention)
-        
-        await ctx.send('le tier {} est associer au rôle(s): {}'.format(tier,' , '.join(C)))
-    else:
-        await ctx.send("usage de la commande: bot!tierSet (int)tier @role @role  ... ")
 
-@bot.command()
-async def initConf(ctx):
-    if path.isdir("/" + guild.name):
-        if path.exist("/" + guild.name + "/server.config"):
-            await ctx.send("les config sont déja intialiser")
+        I_conf(ctx.guild.name)
+
+        Rconfig= open("./" + ctx.guild.name + "/server.json","r")
+        Jconfig=json.load(Rconfig)
+        
+        Tname="Tier "+tier
+        if Tname in Jconfig :
+            Jconfig[Tname]=C
+        else:
+            if "Tier 1" in Jconfig:
+                print(Jconfig["Tier 1"])
+            Jconfig[Tname]=C
+            Rconfig.close()
+            Rconfig=open("./" + ctx.guild.name + "/server.json","w+")
+            json.dump(Jconfig,Rconfig,indent = 4, sort_keys=True)
+
+        await ctx.send('le tier {} est associer au rôle(s): {}'.format(tier,' , '.join(C)))
+        
+    
 
 
 @bot.event
