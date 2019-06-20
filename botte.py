587fd8aab7bd828d667f267427803da76a6748c8
Galyfray <galyfray@gmail.com>
2019-06-20 13:50:28 +0200

    commande tierSet est ok manque le stackage des tier la renomé

--- botte.py
+++ botte.py
@@ -7,16 +7,18 @@ import os
 bot=commands.Bot(command_prefix="bot!")
 
 @bot.command()
-async def tierSet(ctx,tier: int):
+async def tierSet(ctx,tier):
     C=[]
-    print(type(tier))
     if len(ctx.message.role_mentions)==0:
-        await ctx.send("commande invalide veuiller mentioner le role dans la commande")
-    else:
+        await ctx.send("usage de la commande: bot!tierSet (int)tier @role @role  ... \nle rôle doit pouvoir être mentioner")
+    elif type(tier) is int:
         for r in ctx.message.role_mentions:
             C.append(r.mention)
         
         await ctx.send('le tier {} est associer au rôle(s): {}'.format(tier,' , '.join(C)))
+    else:
+        await ctx.send("usage de la commande: bot!tierSet (int)tier @role @role  ... ")
+        
 
 
 @bot.event
