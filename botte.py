4b963e9d24b87e7a82af79bcb23f2ec8e1a792ff
Galyfray <galyfray@gmail.com>
2019-06-24 11:41:36 +0200

    ajout de hierrachie, defaultTier,link et setLink

--- botte.py
+++ botte.py
@@ -19,6 +19,7 @@ def get_token():
         return token
     except:
         print("une erreur est survenue le fichier token.txt n'as pas pus être ouvert")
+        input()
         return ""
 
 
@@ -27,12 +28,12 @@ def conf_op(guild_name: str,mod: str):
         os.mkdir("./" + guild_name)
     if not(os.path.exists("./" + guild_name + "/server.json")):
         f=open("./" + guild_name + "/server.json","w")
-        D={"roles":{},"commands":{}}
+        D={"roles":{},"commands":{},"maxOffset":0}
         json.dump(D,f)
         f.close()
     return open("./" + guild_name + "/server.json",mod)
 
-def conf_load(guild_name: str):
+def conf_load(guild_name: str) -> dict:
     with conf_op(guild_name,"r") as f:
         D=json.load(f)
         f.close()
@@ -43,6 +44,21 @@ def conf_write(guild_name:str,conf:dict):
         json.dump(conf,f,sort_keys=True, indent=4)
         f.close()
 
+def get_max_member_tier(member:discord.Member):
+    conf_dict=conf_load(member.guild.name)
+    role_tier=-math.inf
+    for role in member.roles :
+        if role.name in conf_dict["roles"].keys():
+            role_tier=max((role_tier,int(conf_dict["roles"][role.name])))
+    return role_tier
+
+def get_min_menber_tier(member):
+    conf_dict=conf_load(member.guild.name)
+    role_tier=math.inf
+    for role in member.roles :
+        if role.name in conf_dict["roles"].keys():
+            role_tier=min((role_tier,int(conf_dict["roles"][role.name])))
+    return role_tier
 
 
 #Créeation des commandes:
@@ -51,14 +67,8 @@ def conf_write(guild_name:str,conf:dict):
             description="définit le tier des roles du serveur",
             brief="définit le tier des roles du bot",
             usage="<tier as number> <roles as role.mention list>")
-async def tierSetRole(ctx,tier):
+async def tierSetRole(ctx,tier:int):
     C=[]
-    try:
-        int(tier)
-    except:
-        await ctx.send("commande invalide taper bot!help")
-        return
-    
     if len(ctx.message.role_mentions)==0:
         await ctx.send("commande invalide taper bot!help\nle rôle doit pouvoir être mentioner")
     else:
@@ -77,24 +87,18 @@ async def tierSetRole(ctx,tier):
             description="définit le tier des comamndes du bot",
             brief="définit le tier des comamndes du bot",
             usage="<tier as number> <commands as string list>")
-async def tierSetCommand(ctx,tier,*,command):
+async def tierSetCommand(ctx,tier: int,*,command:str):
     C=[]
-    try:
-        int(tier)
-    except:
-        await ctx.send("commande invalide taper bot!help")
-        return
-    else:
-        conf_dict=conf_load(ctx.guild.name)
-        
-        for cmd in bot.commands :
-            if cmd.name in command :
-                C.append(cmd.name)
-                conf_dict["commands"][cmd.name]=tier
+    conf_dict=conf_load(ctx.guild.name)
+    
+    for cmd in bot.commands :
+        if cmd.name in command :
+            C.append(cmd.name)
+            conf_dict["commands"][cmd.name]=tier
 
-        conf_write(ctx.guild.name,conf_dict)
+    conf_write(ctx.guild.name,conf_dict)
 
-        await ctx.send('le tier {} est associer aux command(s): {}'.format(tier,' , '.join(C)))
+    await ctx.send('le tier {} est associer aux command(s): {}'.format(tier,' , '.join(C)))
 
 @bot.command(aliases=["sdr","setDefaultR","defaulRank","defaultR"],
             description="définit le role par défaut attribuer au utilisateur qui rejoingne le server",
@@ -112,6 +116,55 @@ async def setDefaultRank(ctx):
         conf_write(ctx.guild.name,conf_dict)
         await ctx.send("roles par défault :{}".format(' , '.join(C)))
 
+@bot.command(aliases=["sdt","setDefaultT","defaultT","defaultTier"],
+            description="définit le tier par défault des commandes",
+            brief="définit le tier par défault des commandes",
+            usage="<tier as number>")
+async def setDefaultTier(ctx,tier):
+    try:
+        int(tier)
+    except:
+        await ctx.send("commande invalide taper bot!help pour voir les commandes")
+        return
+    conf_dict=conf_load(ctx.guild.name)
+    for cmd in bot.commands:
+        if not(cmd.name in conf_dict["commands"].keys()):
+            conf_dict["commands"][cmd.name]=tier
+    conf_write(ctx.guild.name,conf_dict)
+    await ctx.send("tier par default des commande définit a: {}\n les configue existance n'on pas été écraser".format(tier))
+
+@bot.command(aliases=["lien"],
+            description="renvoie un lien définit par setLink",
+            brief="renvoie un lien définit par setLink",
+            usage="")
+async def link(ctx):
+    conf_dict=conf_load(ctx.guild.name)
+    if "link" in conf_dict.keys():
+        await ctx.send(conf_dict["link"])
+    else:
+        await ctx.send("aucun lien n'as été définit :/")
+
+@bot.command(aliases=["sl","setL","setLien"],
+            description="définit le lien de la commande link, un message peut être ajouter",
+            brief="définit le lien pour link",
+            usage="<link as string>")
+async def setLink(ctx,*,link):
+    conf_dict=conf_load(ctx.guild.name)
+    conf_dict["link"]=link
+    conf_write(ctx.guild.name,conf_dict)
+    await ctx.send("lien sauvegarder :)")
+
+@bot.command(aliases=["vtf","hset","hiérach","h"],
+            description="définit l'écart max de tier entre une personne mentionnant et le mentioner" + \
+            "\nexpl:si définit a 1 alors si tier 1 mentione tier2 ok si tier 1 mentionne tier 3 pasok" + \
+            "\nvaleur négative ou 0 pour désactiver la fonction",
+            brief="définit l'écart max de mentionnement autoriser",
+            usage="<offset as number>")
+async def hiérarchie(ctx,offset: int):
+    conf_dict = conf_load(ctx.guild.name)
+    conf_dict["maxOffset"]=offset or 0
+    await ctx.send("Offset définit a {}".format(offset))
+
 #Event du bot
 
 @bot.event
@@ -125,6 +178,9 @@ async def on_command_error(ctx,error):
     elif isinstance(error,commands.CommandNotFound):
         await ctx.send("commande inexistante taper bot!help pour voir la liste des commandes")
         return
+    elif isinstance(error,commands.BadArgument):
+        await ctx.send("argument invalide taper bot!help {} pour voir le type des argument".format(ctx.command))
+        return
     print('Ignoring exception in command {}:'.format(ctx.command), file=sys.stderr)
     traceback.print_exception(type(error), error, error.__traceback__, file=sys.stderr)
 
@@ -133,7 +189,7 @@ async def on_member_join(member):
     conf_dict=conf_load(member.guild.name)
     if "defaltRank" in conf_dict:
         for role in conf_dict["defaltRank"] :
-            member.add_roles(discord.utils.get(member.guild.roles,name=role))
+            await member.add_roles(discord.utils.get(member.guild.roles,name=role))
     
 @bot.check
 async def checkTier(ctx):
@@ -141,21 +197,40 @@ async def checkTier(ctx):
     if(ctx.message.author.guild_permissions.administrator):
         return True
     elif ctx.command.name in conf_dict["commands"].keys():
-        test=False
-        for role in ctx.message.author.roles :
-            if role.name in conf_dict["roles"].keys():
-                if int(conf_dict["roles"][role.name])>=int(conf_dict["commands"][ctx.command.name]):
-                    test = True 
-        return test
+        if get_max_member_tier(ctx.message.author)>=int(conf_dict["commands"][ctx.command.name]):
+            return True
+        else:
+            return False
     else:
         return False
 
 @bot.event
 async def on_message(message):
     await bot.process_commands(message)
+    conf_dict=conf_load(message.guild.name)
+    if not(message.author.guild_permissions.administrator) and int(conf_dict["maxOffset"])!=0 and (len(message.mentions)>=0 or len(message.role_mentions)>=0):
+        author_tier=get_max_member_tier(message.author)
+        test=True
+        if len(message.mentions)>=0:
+            for member in message.mention:
+                if author_tier < get_min_menber_tier(member):
+                    test=False
+        if len(message.role_mention)>=0:
+            for role in message.role_mention:
+                if role.name in conf_dict["roles"].key():
+                    if author_tier<int(conf_dict["roles"][role.name]):
+                        test=False
+    if not(test):
+        message.delete
+
+        
+        
+        
+
 
 @bot.event
 async def on_ready():
     print("bot pret a botter des gens !")
 
 bot.run(get_token())
+input()
